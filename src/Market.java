28e2ba3cb32634c64c85597a3beeacfc16bf10ca
aliciapirani <apiranilove@gmail.com>
2025-04-15 20:55:19 -0400

    added to the customer menu

--- src/Market.java
+++ src/Market.java
@@ -32,13 +32,56 @@ public class Market {
 		System.out.println("Thank you for visiting our Farmers Market!");
 	}
 
+	//customer menu 
 	private void customerMenu() {
-		// TODO Auto-generated method stub
-		
+		while (true) { 
+			System.out.println("\n" + "Welcome Customer! Please make a selection from the list below.");
+			System.out.println("1. View Vendors and Products");
+			System.out.println("2. Leave a Product Review");
+			System.out.println("3. Back");
+			System.out.println("Choose: ");
+			int choice = scnr.nextInt();
+			scnr.nextLine();
+			
+			if (choice == 3) break;
+			
+			else if (choice == 1) {
+				if (vendors.isEmpty()) {
+					System.out.println("No vendors available.");
+					continue;
+				}
+				
+				System.out.println("\n" + "Vendor List: ");
+				for (int i = 0; i < vendors.size(); i++) {
+					System.out.println((i + 1) + ". " + vendors.get(i).getName());
+				}
+				
+				System.out.println("Select vendor by number: ");
+				int vendorIndex = scnr.nextInt() - 1;
+				scnr.nextLine();
+				
+				if (vendorIndex < 0 || vendorIndex >= vendors.size()) {
+					System.out.println("Invalid vendor selection.");
+					continue;
+				}
+				
+				Vendor selectedVendor = vendors.get(vendorIndex);
+				System.out.println("\n" + "Product from " + selectedVendor.getName() + ":");
+				selectedVendor.displayInventory();
+			}
+			
+			else if (choice == 2) {
+				//LEFT OFF HERE
+				
+			}
+			
+			System.out.println("Thank you for visiting our Farmers Market!");
+		}
+			
 	}
 	
+	//vendor menu
 	private void vendorMenu() {
-		// TODO Auto-generated method stub
 		
 	}
 }
